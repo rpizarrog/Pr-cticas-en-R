4530f8be942f22815ea4148e946584c4a89d49ce
rpizarrog <rpizarro@itdurango.edu.mx>
2021-09-21 09:58:31 -0500

    Add files via upload

---
+++ funciones/misfunciones.R
@@ -0,0 +1,12 @@
+# Funciones
+media_geom <- function (valores) {
+  n <- length(valores)
+  multiplicacion <- 1
+  for (var in numeros) {
+    # print(var)
+    multiplicacion <- multiplicacion * var
+  }
+  multiplicacion
+  media_geometrica <- multiplicacion ^ (1/n)
+  media_geometrica
+}
