94c6c2bac31cea0b92860ded2b83956c17cb07c6
rpizarrog <rpizarro@itdurango.edu.mx>
2021-09-21 10:11:48 -0500

    Create llamar_funciones.r

---
+++ funciones/llamar_funciones.r
@@ -0,0 +1,10 @@
+# Como cargar funciones en R
+# Como mandarlas llamar
+# Ejemplo mandar llamar el programa misfunciones.r que contiene la lfución media_geom()
+
+source("https://raw.githubusercontent.com/rpizarrog/Trabajos-en-R-AD2021/main/funciones/misfunciones.R")
+
+media_geom(valores = c(34,54,34,56,57))
+
+
+[1] 45.6965
